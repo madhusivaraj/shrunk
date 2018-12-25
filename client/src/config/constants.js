cf599a95e8ff3c7ff804652bda59b014f6031130
Madhu Sivaraj <madhuksivaraj@gmail.com>
2018-12-25 02:14:20 -0500

    attempting to deploy

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
-  apiUrl: "http://localhost:7000/",
-  baseUrl: "http://localhost:7000/url/"
-  //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
-  // baseUrl: "http://muhzi.com"
+  /*apiUrl: "http://localhost:7000/",
+  baseUrl: "http://localhost:7000/url/"*/
+
+  apiUrl: "http://madhusivaraj.com/shrunk/",
+  baseUrl: "http://madhusivaraj.com/shrunk/url/"
 };
