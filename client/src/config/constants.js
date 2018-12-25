11c4e988f7a4a913562bd67715f6b813bb296957
Madhu Sivaraj <madhuksivaraj@gmail.com>
2018-12-25 01:19:05 -0500

    shortener works yay

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: "http://localhost:7000/api/",
-  baseUrl: "http://localhost:7000/api/item/"
+  apiUrl: "http://localhost:7000/",
+  baseUrl: "http://localhost:7000/item/"
   //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
   // baseUrl: "http://muhzi.com"
 };
